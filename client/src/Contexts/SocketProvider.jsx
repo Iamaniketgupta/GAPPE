3c98bbdc765a6e98c91230a10011beb7ea3f4095
Aniket Gupta <aniketgupta.org@gmail.com>
2024-10-27 18:34:56 +0530

    update

--- client/src/Contexts/SocketProvider.jsx
+++ client/src/Contexts/SocketProvider.jsx
@@ -10,7 +10,7 @@ export const SocketProvider = ({ children }) => {
     const [currUser] = useRecoilState(userData);
     
     useEffect(() => {
-        const newSocket = io('https://gappe.onrender.com');
+        const newSocket = io('https://gappe-server.vercel.app');
         setSocket(newSocket);
  
       if(!newSocket) return;
