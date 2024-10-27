4c5ee8b834bd4b95c1448b393bf6e0ad840f00ff
Aniket Gupta <115345548+Iamaniketgupta@users.noreply.github.com>
2024-10-27 16:52:25 +0530

    Update Home.jsx

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar.jsx';
 import HeroSection from './components/HeroSection';
 import CTA from './components/CTA';
 import FAQ from './components/FAQ';
