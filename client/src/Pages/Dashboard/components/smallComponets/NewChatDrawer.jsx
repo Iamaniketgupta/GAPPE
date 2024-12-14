f4833b0670164d7e6f2c80b9b5027eee489c01e5
Aniket Gupta <aniketgupta.org@gmail.com>
2024-12-15 00:13:57 +0530

    fix scroll

--- client/src/Pages/Dashboard/components/smallComponets/NewChatDrawer.jsx
+++ client/src/Pages/Dashboard/components/smallComponets/NewChatDrawer.jsx
@@ -11,10 +11,10 @@ import { IoArrowBackSharp } from 'react-icons/io5';
 
 
 const NewChatDrawer = () => {
-    const { allUsers, setAllUsers ,handleFetchChats,usersChatLoading} = useChatContext();
+    const { allUsers, setAllUsers, handleFetchChats, usersChatLoading } = useChatContext();
     const [loading, setLoading] = useState(false);
     const [accessChatLoading, setAcessChatLoading] = useState(false);
-    const [currSelectedChat,setCurrSelectedChat] = useRecoilState(accessedChat);
+    const [currSelectedChat, setCurrSelectedChat] = useRecoilState(accessedChat);
     const [value, setValue] = useRecoilState(commonDrawer)
 
 
@@ -34,8 +34,8 @@ const NewChatDrawer = () => {
     const handleAccessChat = (userId) => {
         setAcessChatLoading(true);
         accessChat(userId).then((res) => {
-                setCurrSelectedChat(res.chat[0]);
-                handleFetchChats()
+            setCurrSelectedChat(res.chat[0]);
+            handleFetchChats()
         }).catch((err) => {
             console.log(err);
         }).finally(() => {
@@ -48,7 +48,7 @@ const NewChatDrawer = () => {
             setAllUsers(allUsers?.users);
         })
     }, []);
-    
+
     return (
         <div>
             {/* header */}
@@ -61,7 +61,7 @@ const NewChatDrawer = () => {
             <div className='my-4'>
                 <SearchInp onChange={onSearch} placeholder="Search users" />
             </div>
-            <div onClick={()=>setValue("CreateGroup")}  className='mt-4  mb-1 flex cursor-pointer hover:bg-stone-100 dark:hover:bg-stone-700 py-2 items-center gap-4'>
+            <div onClick={() => setValue("CreateGroup")} className='mt-4  mb-1 flex cursor-pointer hover:bg-stone-100 dark:hover:bg-stone-700 py-2 items-center gap-4'>
                 <div className='bg-blue-500 text-white rounded-full w-12 h-12 flex items-center justify-center'>
                     <MdGroup size={24} className='' />
                 </div>
@@ -70,32 +70,34 @@ const NewChatDrawer = () => {
 
             <div className='dark:bg-stone-700 bg-gray-200 w-full h-[1px]' />
             {/* All Users */}
-            {
-                usersChatLoading ?
-                    (<div className='flex overflow-y-auto  relative max-h-[calc(100vh-100px)] flex-col gap-1 mt-2'
-                        style={{ scrollbarWidth: 'thin', scrollbarColor: 'blue' }}>
-                        {Array(5).fill().map((_, index) => <UsersListShimmer key={index} />)}
-                    </div>)
-                    : (<div className='flex overflow-y-auto relative max-h-[calc(100vh-100px)] flex-col gap-1 mt-2'
-                        style={{ scrollbarWidth: 'thin', scrollbarColor: 'blue' }}>
-                        {allUsers?.map((item) =>
-                            <div key={item._id} onClick={()=>handleAccessChat(item._id)} className='relative flex items-center hover:bg-stone-100 dark:hover:bg-stone-700  gap-5 cursor-pointer py-2 bg-white dark:bg-stone-800 dark:text-white'
-                            >
-                                <img src={item.avatar} alt={item.fullName}
-                                    className='w-12 h-12  bg-black rounded-full ' />
-                                <div>
-                                    <h3 className='text-md  font-semibold'>
-                                        {item.fullName}
-                                    </h3>
-                                    <p className='text-xs text-gray-400'>
-                                        {item.email}
-                                    </p>
-                                </div>
-                            </div>)
-                        }
-                    </div>)
-            }
 
+            <div className='min-h-full overflow-y-scroll'>
+                {
+                    usersChatLoading ?
+                        (<div className='flex overflow-y-auto  relative max-h-[calc(100vh-100px)] flex-col gap-1 mt-2'
+                            style={{ scrollbarWidth: 'thin', scrollbarColor: 'blue' }}>
+                            {Array(5).fill().map((_, index) => <UsersListShimmer key={index} />)}
+                        </div>)
+                        : (<div className='flex overflow-y-auto relative max-h-[calc(100vh-100px)] flex-col gap-1 mt-2'
+                            style={{ scrollbarWidth: 'thin', scrollbarColor: 'blue' }}>
+                            {allUsers?.map((item) =>
+                                <div key={item._id} onClick={() => handleAccessChat(item._id)} className='relative flex items-center hover:bg-stone-100 dark:hover:bg-stone-700  gap-5 cursor-pointer py-2 bg-white dark:bg-stone-800 dark:text-white'
+                                >
+                                    <img src={item.avatar} alt={item.fullName}
+                                        className='w-12 h-12  bg-black rounded-full ' />
+                                    <div>
+                                        <h3 className='text-md  font-semibold'>
+                                            {item.fullName}
+                                        </h3>
+                                        <p className='text-xs text-gray-400'>
+                                            {item.email}
+                                        </p>
+                                    </div>
+                                </div>)
+                            }
+                        </div>)
+                }
+            </div>
         </div >
     );
 }
